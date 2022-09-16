8ac73d4151228d4f11c120d8d5b879f241379e68
BreynerParada <breyner.parada@untoquedigital.com>
2022-09-16 09:29:44 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,57 @@
+// const express = require('express');
+
+// const app = express();
+// const port = 3000;
+// const server = require('http').Server(app);
+// const io = require('socket.io')(server)
+
+
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const router = require('./router');
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./users');
+
+const port = process.env.PORT || 3001;
+const app = express();
+const server = http.createServer(app);
+const io = socketio (server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
+
+app.use(express.json());
+app.use(router);
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
+    socket.on('join', ({name, room}, cb) => {
+        const {error, user} = addUser({id: socket.id, name, room});
+        
+        if (error) return cb(error);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, Welcome to the room ${user.room}` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined the room ${user.room}` });
+
+        socket.join(user.room);
+
+        cb();
+    })
+    socket.on('sendMessage', (message , cb) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+
+        cb();
+    });
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
