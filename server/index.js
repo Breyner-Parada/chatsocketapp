a62dfbf5082e9ae5f8b2894db37cf459d7f88c78
BreynerParada <breyner.parada@untoquedigital.com>
2022-09-19 15:46:17 -0500

    infobar and messages added

--- server/index.js
+++ server/index.js
@@ -5,53 +5,67 @@
 // const server = require('http').Server(app);
 // const io = require('socket.io')(server)
 
-
-const express = require('express');
-const socketio = require('socket.io');
-const http = require('http');
-const router = require('./router');
-const {addUser, removeUser, getUser, getUsersInRoom} = require('./users');
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const router = require("./router");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
 const port = process.env.PORT || 3001;
 const app = express();
 const server = http.createServer(app);
-const io = socketio (server, {
-    cors: {
-        origin: '*',
-        methods: ["GET", "POST"],
-        credentials: true
-    }
+const io = socketio(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
 
 app.use(express.json());
 app.use(router);
 
-io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('join', ({name, room}, cb) => {
-        const {error, user} = addUser({id: socket.id, name, room});
-        
-        if (error) return cb(error);
+io.on("connection", (socket) => {
+  console.log("Client connected");
+  socket.on("join", ({ name, room }, cb) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
 
-        socket.emit('message', { user: 'admin', text: `${user.name}, Welcome to the room ${user.room}` });
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined the room ${user.room}` });
+    if (error) return cb(error);
+    socket.join(user.room);
 
-        socket.join(user.room);
-
-        cb();
-    })
-    socket.on('sendMessage', (message , cb) => {
-        const user = getUser(socket.id);
-
-        io.to(user.room).emit('message', { user: user.name, text: message });
-
-        cb();
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, Welcome to the room ${user.room}`,
     });
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
-    })
-})
+    socket.broadcast
+      .to(user.room)
+      .emit("message", {
+        user: "admin",
+        text: `${user.name} has joined the room ${user.room}`,
+      });
+
+      io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
+
+    cb();
+  });
+  socket.on("sendMessage", (message, cb) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    io.to(user.room).emit("roomData", { room: user.room, users: getUsersInRoom(user.room) });
+
+    cb();
+  });
+  socket.on("disconnect", () => {
+    console.log('User disconnected');
+   const user = removeUser(socket.id);
+
+   if(user) {
+    io.to(user.room).emit("message", { user: 'admin', text: `${user.name} has disconnected` });
+   }
+  });
+});
 
 server.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-})
+  console.log(`Listening on port ${port}`);
+});
